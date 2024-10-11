f23735095eaff9f6609405951e934af6987eb6d2
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2024-10-10 21:18:17 -0600

    Add Alp

---
+++ 1.5/Source/MedievalOverhaul/Medieval Overhaul/DamageWorkers/DamageWorker_AlpSleep.cs
@@ -0,0 +1,32 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace MedievalOverhaul
+{
+    public class DamageWorker_AlpSleep : DamageWorker
+    {
+        public override DamageWorker.DamageResult Apply(DamageInfo dinfo, Thing victim)
+        {
+            DamageWorker.DamageResult result = new DamageWorker.DamageResult();
+            Pawn pawn = victim as Pawn;
+            if (pawn != null && pawn.needs.rest != null)
+            {
+                float sleepDamage = dinfo.Amount / 100f;
+                float newRestLevel = pawn.needs.rest.curLevelInt - sleepDamage;
+                pawn.needs.rest.curLevelInt = newRestLevel < 0 ? 0 : newRestLevel;
+                if (pawn.needs.rest.curLevelInt < 0.3f)
+                {
+                    Hediff hediff = HediffMaker.MakeHediff(dinfo.Def.hediff, pawn, null);
+                    hediff.Severity = 1f;
+                    pawn.health.AddHediff(hediff, null, new DamageInfo?(dinfo), null);
+                }
+            }
+            return result;
+        }
+    }
+}
