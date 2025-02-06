e97c1d43d5690c460d450a0c4dfc3daea49b4fc5
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2025-02-05 20:55:28 -0700

    Water Gathering Spot
    * Gathering spot replaces designation zone

---
+++ 1.5/Source/MedievalOverhaul/Medieval Overhaul/PlaceWorker/PlaceWorker_WaterGather.cs
@@ -0,0 +1,27 @@
+﻿using RimWorld;
+using Verse;
+
+namespace MedievalOverhaul
+{
+    public class PlaceWorker_WaterGather : PlaceWorker
+    {
+
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+        {
+            foreach (IntVec3 c in GenAdj.CellsOccupiedBy(loc, rot, checkingDef.Size))
+            {
+                if (!c.InBounds(map))
+                {
+                    return new AcceptanceReport("DankPyon_InBound".Translate());
+                }
+                var terrain = map.terrainGrid.TerrainAt(c);
+                if (!terrain.IsWater && terrain.defName != "Ice")
+                {
+                    return new AcceptanceReport("DankPyon_NeedsWater".Translate());
+                }
+            }
+            return AcceptanceReport.WasAccepted;
+        }
+
+    }
+}
