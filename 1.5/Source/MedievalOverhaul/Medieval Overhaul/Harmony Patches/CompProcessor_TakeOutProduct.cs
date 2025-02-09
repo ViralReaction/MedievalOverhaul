01522d186b3cc188edf65d28af905dfc224cd251
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2025-02-09 14:55:22 -0700

    Lumber Mills Process Use Butcher Product

--- 1.5/Source/MedievalOverhaul/Medieval Overhaul/Harmony Patches/CompProcessor_TakeOutProduct.cs
+++ 1.5/Source/MedievalOverhaul/Medieval Overhaul/Harmony Patches/CompProcessor_TakeOutProduct.cs
@@ -34,13 +34,13 @@ namespace MedievalOverhaul.Patches
                                         count = amountLeather,
                                         thingDef = comp.Props.leatherType
                                     };
-                                    __result = TakeOutButcherProduct(__instance, thingDefCount, activeProcess);
+                                    __result = TakeOutButcherProduct(__instance, thing, thingDefCount, activeProcess);
                                     return false;
                                 }
                                 else if (thing.def.butcherProducts?.Any() ?? false)
                                 {
                                     var thingDefCount = thing.def.butcherProducts.FirstOrDefault();
-                                    __result = TakeOutButcherProduct(__instance, thingDefCount, activeProcess);
+                                    __result = TakeOutButcherProduct(__instance, thing, thingDefCount, activeProcess);
                                     return false;
                                 }
                             }
@@ -65,13 +65,13 @@ namespace MedievalOverhaul.Patches
             }
         }
 
-        public static Thing TakeOutButcherProduct(CompProcessor __instance, ThingDefCountClass thingDefCount, ActiveProcess activeProcess)
+        public static Thing TakeOutButcherProduct(CompProcessor __instance, Thing ingredientThing, ThingDefCountClass thingDefCount, ActiveProcess activeProcess)
         {
             Thing thing = null;
             if (!activeProcess.Ruined)
             {
                 thing = ThingMaker.MakeThing(thingDefCount.thingDef);
-                thing.stackCount = thingDefCount.count;
+                thing.stackCount = thingDefCount.count * ingredientThing.stackCount;
             }
             foreach (Thing ingredientThing2 in activeProcess.ingredientThings)
             {
