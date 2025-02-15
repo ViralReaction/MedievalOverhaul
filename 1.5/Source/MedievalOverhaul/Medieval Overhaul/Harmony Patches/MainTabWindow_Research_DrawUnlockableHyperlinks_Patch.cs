c7bff83dc4e877c2186b0c9ba1cc1a06cf7ad82f
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2025-02-15 13:59:58 -0700

    Fix Research Page
    * Fixes mod source on the research page for research that have schematics.

--- 1.5/Source/MedievalOverhaul/Medieval Overhaul/Harmony Patches/MainTabWindow_Research_DrawUnlockableHyperlinks_Patch.cs
+++ 1.5/Source/MedievalOverhaul/Medieval Overhaul/Harmony Patches/MainTabWindow_Research_DrawUnlockableHyperlinks_Patch.cs
@@ -14,11 +14,13 @@ namespace MedievalOverhaul.Patches
     [HarmonyPatch(typeof(MainTabWindow_Research), "DrawUnlockableHyperlinks")]
     public static class MainTabWindow_Research_DrawUnlockableHyperlinks_Patch
     {
-        public static void Prefix(MainTabWindow_Research __instance, ref Rect rect, Rect visibleRect, ResearchProjectDef project)
+        public static void Prefix(MainTabWindow_Research __instance, ref Rect rect, Rect visibleRect, ResearchProjectDef project, out float __state)
         {
             var extension = project.GetModExtension<RequiredSchematic>();
+            __state = 0f;
             if (extension != null)
             {
+                __state = rect.yMin;
                 Widgets.LabelCacheHeight(ref rect, "DankPyon_RequiredSchematic".Translate() + ":");
                 Rect rect2 = new (rect.x, rect.yMin + 24, rect.width, 24f);
                 var item2 = extension.schematicDef;
@@ -45,5 +47,10 @@ namespace MedievalOverhaul.Patches
                 rect.yMin += 24f;
             }
         }
+        public static void Postfix(MainTabWindow_Research __instance, ref float __result, ref Rect rect, float __state)
+        {
+            __result = rect.yMin - __state; // Return the updated yMin as the final result
+        }
+
     }
 }
