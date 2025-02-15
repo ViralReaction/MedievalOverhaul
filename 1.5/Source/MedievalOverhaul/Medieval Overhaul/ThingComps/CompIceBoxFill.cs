cc0c212ffaf3d339def02b41b6def65f14bf48df
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2025-02-14 17:29:20 -0700

    Ice Box Weather Fill
    * Ice Box will now slowly fill in rain or snow

---
+++ 1.5/Source/MedievalOverhaul/Medieval Overhaul/ThingComps/CompIceBoxFill.cs
@@ -0,0 +1,69 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+using ProcessorFramework;
+using System.Linq;
+
+namespace MedievalOverhaul
+{
+    public class CompIceBoxFill : ThingComp
+    {
+        private Thing _parent;
+        private Map _map;
+        private CompProcessor _compProcessor;
+        private float _rainAccumulated = 0f;
+        private const float RAINTHRESHOLD = 5f;
+        private const float SNOWMULTIPLIER = 1f;
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+
+            _parent = this.parent;
+            _map = _parent.Map;
+            _compProcessor = _parent.TryGetComp<CompProcessor>();
+            if (_compProcessor == null)
+            {
+                Log.Error( "Medieval Overhaul: " + _parent.def + " " + "is missing CompProcessor and will be despawned.");
+                _parent.Destroy();
+            }    
+
+
+        }
+        public override void CompTickRare()
+        {
+            if (_compProcessor.SpaceLeft == 0) return;
+            if (_map.weatherManager.RainRate == 0 && _map.weatherManager.SnowRate == 0) return;
+            if (!_parent.IsOutside() || _parent.Position.Roofed(_map)) return;
+
+
+            float rainRate = _map.weatherManager.RainRate;
+            float snowRate = _map.weatherManager.SnowRate;
+            _rainAccumulated += rainRate + (snowRate * SNOWMULTIPLIER);
+            if (_rainAccumulated < RAINTHRESHOLD) return;
+
+            _rainAccumulated = 0f;
+
+            var processList = _compProcessor.enabledProcesses;
+            if (processList.Count == 0) return;
+            foreach (var processDef in processList)
+            {
+                var process = processDef.Key;
+                var ingredientFilter = processDef.Value;
+                ThingDef ingredient = null;
+                for (int i = 0; i < ingredientFilter.allowedIngredients.Count; i++)
+                {
+                    if (ingredientFilter.allowedIngredients[i] != null)
+                    {
+                        ingredient = ingredientFilter.allowedIngredients[i];
+                        break;
+                    }
+                }
+                if (ingredient == null) return;
+                Thing thing = ThingMaker.MakeThing(ingredient);
+                thing.stackCount = 1;
+                _compProcessor.AddIngredient(thing, process);
+                break;
+            }
+        }
+
+    }
+}
