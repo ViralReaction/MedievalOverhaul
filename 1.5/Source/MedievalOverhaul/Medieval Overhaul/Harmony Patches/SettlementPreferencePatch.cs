c8d17d85cbb84faed6353f8f9be2b68462d806ec
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2025-02-17 17:40:16 -0700

    Settlement Generation Patch Readability

--- 1.5/Source/MedievalOverhaul/Medieval Overhaul/Harmony Patches/SettlementPreferencePatch.cs
+++ 1.5/Source/MedievalOverhaul/Medieval Overhaul/Harmony Patches/SettlementPreferencePatch.cs
@@ -5,14 +5,27 @@ using Verse;
 
 namespace MedievalOverhaul
 {
+    [HarmonyPatch(typeof(TileFinder), "RandomSettlementTileFor")]
     public static class SettlementPreferencePatch
     {
-        [HarmonyPatch(typeof(TileFinder))]
-        [HarmonyPatch("RandomSettlementTileFor")]
-        public static class TileFinder_RandomSettlementTileFor_Patch
+
+        public static bool Prefix(ref int __result, Faction faction)
         {
-            [HarmonyPrefix]
-            public static bool SettlementPatch(ref int __result, Faction faction) => !MedievalOverhaulSettings.settings.StandaloneSettlementPreference_EnableSettlementPreference || faction == null || SettlementPreference.Get((Def)faction.def) == null || !Rand.Chance(SettlementPreference.Get((Def)faction.def).chance) || SettlementPreferenceUtility.GetTileID(faction, out __result);
+            // If the setting to enable settlement preference is disabled, return true
+            if (!MedievalOverhaulSettings.settings.StandaloneSettlementPreference_EnableSettlementPreference || faction is null)
+                return true;
+
+
+            // Get the settlement preference for this faction definition
+            var preference = SettlementPreference.Get((Def)faction.def);
+
+            if (preference is null || !Rand.Chance(preference.chance))
+            {
+                return true;
+            }
+
+            // Attempt to get the tile ID; return its result
+            return SettlementPreferenceUtility.GetTileID(faction, out __result);
         }
     }
 }
