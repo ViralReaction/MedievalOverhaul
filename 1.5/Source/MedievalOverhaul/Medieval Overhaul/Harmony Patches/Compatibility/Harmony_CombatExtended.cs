a0f73b3d7e5482d58fb1b14615830c603f7d8435
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2025-02-28 13:26:04 -0700

    Update Harmony Patches

---
+++ 1.5/Source/MedievalOverhaul/Medieval Overhaul/Harmony Patches/Compatibility/Harmony_CombatExtended.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace MedievalOverhaul.Patches
+{
+
+    public static class Harmony_CombatExtended
+    {
+        private static Type Verb_MeleeAttackCE_Patch
+        {
+            get
+            {
+                return AccessTools.TypeByName("CombatExtended.Verb_MeleeAttackCE");
+            }
+        }
+        [HarmonyPatch]
+        public static class Verb_MeleeAttackCE_ApplyMeleeDamageToTarget
+        {
+            public static bool Prepare()
+            {
+                return Verb_MeleeAttackCE_Patch != null;
+            }
+
+            public static MethodBase TargetMethod()
+            {
+                return AccessTools.Method("CombatExtended.Verb_MeleeAttackCE:ApplyMeleeDamageToTarget");
+            }
+
+            public static void Postfix(ref DamageWorker.DamageResult __result, Verb_MeleeAttack __instance, LocalTargetInfo target)
+            {
+                if (__result.totalDamageDealt == 0) return;
+
+                Pawn attacker = __instance.CasterPawn;
+                if (attacker?.equipment?.Primary is ThingWithComps weapon)
+                {
+                    CompWeaponOil oilComp = weapon.TryGetComp<CompWeaponOil>();
+                    if (oilComp != null && oilComp.oilCharges > 0)
+                    {
+                        if (__instance.currentTarget.Thing is Pawn victim)
+                        {
+                            var list = __result.hediffs;
+                            bool lethalFlag = false;
+                            foreach ( Hediff hediff in list)
+                            {
+                                if (hediff.IsLethal && hediff.Severity > 0)
+                                {
+                                    lethalFlag = true;
+                                    break;
+                                }
+                            }
+                            if (lethalFlag == true)
+                            {
+                                victim.health.AddHediff(oilComp.hediffDef);
+                                oilComp.UseCharge();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
--- 1.5/Source/MedievalOverhaul/Medieval Overhaul/ThingComps/NewFolder1/VerbMeleeAttack_TryCastShot.cs
+++ 1.5/Source/MedievalOverhaul/Medieval Overhaul/ThingComps/NewFolder1/VerbMeleeAttack_TryCastShot.cs
@@ -4,12 +4,35 @@ using Verse;
 
 namespace MedievalOverhaul.Patches
 {
-    [HarmonyPatch(typeof(Verb_MeleeAttack), "TryCastShot")]
-    public static class VerbMeleeAttack_TryCastShot
+    //[HarmonyPatch(typeof(Verb_MeleeAttack), "TryCastShot")]
+    //public static class VerbMeleeAttack_TryCastShot
+    //{
+    //    public static void Postfix(bool __result, Verb_MeleeAttack __instance)
+    //    {
+    //        if (!__result) return;
+
+    //        Pawn attacker = __instance.CasterPawn;
+    //        if (attacker?.equipment?.Primary is ThingWithComps weapon)
+    //        {
+    //            CompWeaponOil oilComp = weapon.TryGetComp<CompWeaponOil>();
+    //            if (oilComp != null && oilComp.oilCharges > 0)
+    //            {
+    //                if (__instance.currentTarget.Thing is Pawn target)
+    //                {
+    //                    target.health.AddHediff(oilComp.hediffDef);
+    //                    oilComp.UseCharge();
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
+
+    [HarmonyPatch(typeof(Verb_MeleeAttackDamage), "ApplyMeleeDamageToTarget")]
+    public static class VerbMeleeAttack_ApplyMeleeDamageToTarget
     {
-        public static void Postfix(bool __result, Verb_MeleeAttack __instance)
+        public static void Postfix(DamageWorker.DamageResult __result, Verb_MeleeAttackDamage __instance)
         {
-            if (!__result) return;
+            if (__result.totalDamageDealt == 0) return;
 
             Pawn attacker = __instance.CasterPawn;
             if (attacker?.equipment?.Primary is ThingWithComps weapon)
@@ -17,10 +40,24 @@ namespace MedievalOverhaul.Patches
                 CompWeaponOil oilComp = weapon.TryGetComp<CompWeaponOil>();
                 if (oilComp != null && oilComp.oilCharges > 0)
                 {
-                    if (__instance.currentTarget.Thing is Pawn target)
+                    if (__instance.currentTarget.Thing is Pawn victim)
                     {
-                        target.health.AddHediff(oilComp.hediffDef);
-                        oilComp.UseCharge();
+                        var list = __result.hediffs;
+                        bool lethalFlag = false;
+                        foreach (Hediff hediff in list)
+                        {
+                            if (hediff.IsLethal && hediff.Severity > 0)
+                            {
+                                
+                                lethalFlag = true;
+                                break;
+                            }
+                        }
+                        if (lethalFlag == true)
+                        {
+                            victim.health.AddHediff(oilComp.hediffDef);
+                            oilComp.UseCharge();
+                        }
                     }
                 }
             }
