73af60717d68d3b9886871311e9fa504b545d836
Alias <25497382+Alias44@users.noreply.github.com>
2025-06-18 19:54:31 -0700

    Added BackCompatibilityConverter
    Added code to handle the removed tent wall def.

---
+++ 1.6/Source/MedievalOverhaul/Medieval Overhaul/Harmony Patches/Compatibility/BackCompatibilityInject.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using Verse;
+
+namespace MedievalOverhaul.Patches
+{
+    public class BackCompatibilityInject : Mod
+    {
+        public BackCompatibilityInject(ModContentPack content) : base(content)
+        {
+            List<BackCompatibilityConverter> compatibilityConverters =
+    AccessTools.StaticFieldRefAccess<List<BackCompatibilityConverter>>(typeof(BackCompatibility),
+        "conversionChain");
+
+            compatibilityConverters.Add(new BackCompatibilityConverter_TentWalls());
+        }
+    }
+}
