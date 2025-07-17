96021c54e2843616e230c7007c2ad6e6708debfb
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2025-07-17 15:31:37 -0600

    Biome Temp Validator Hornet Nest

---
+++ 1.6/Source/MedievalOverhaul/Medieval Overhaul/MapGeneration/ScattererValidator_NestTemp.cs
@@ -0,0 +1,12 @@
+﻿using Verse;
+namespace MedievalOverhaul
+{
+    public class ScattererValidator_NestTemp : ScattererValidator
+    {
+        public override bool Allows(IntVec3 c, Map map)
+        {
+            return map.TileInfo.temperature is >= -10f and <= 40f;
+        }
+
+    }
+}
